8d38c69c405bc5a6a2b52bd1526a2f8fb3fe7a19
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2016-07-22 10:14:56 +0200

    Added genome flexibility

--- R/getgc.R
+++ R/getgc.R
@@ -1,4 +1,4 @@
-getgc <- function(chr, ref) {
+getgc <- function(chr, ref, genome = BSgenome.Hsapiens.UCSC.hg19) {
     if (chr == "X" | chr == "x" | chr == "chrX" | chr == "chrx") {
         chrtemp <- 23
     } else if (chr == "Y" | chr == "y" | chr == "chrY" | chr == "chry") {
@@ -7,8 +7,8 @@ getgc <- function(chr, ref) {
         chrtemp <- as.numeric(mapSeqlevels(as.character(chr), "NCBI")[1])
     }
     if (length(chrtemp) == 0) 
-        message("Chromosome cannot be found in NCBI Homo sapiens database!")
-    chrm <- unmasked(Hsapiens[[chrtemp]])
+        message("Chromosome name cannot be mapped to NCBI style!")
+    chrm <- unmasked(genome[[chrtemp]])
     seqs <- Views(chrm, ref)
     af <- alphabetFrequency(seqs, baseOnly = TRUE, as.prob = TRUE)
     gc <- round((af[, "G"] + af[, "C"]) * 100,2)
